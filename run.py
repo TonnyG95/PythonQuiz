c2d1a8c4559c52850c9536b469ec2da3ba483866
TonnyG95 <galic_antonio@hotmail.com>
2022-02-03 10:47:51 +0000

    first commit affter pep8

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 def welcome_player():
-        """
-Display welcome message, store user name and return greeting
+    """
+    Display welcome message, store user name and return greeting
+    """
 
-"""
 
 print(
     """
@@ -18,48 +18,74 @@ print(
 
 name = input("How do you want me to call you? ")
 name = name.capitalize()
+
+
 print("Okay " + name + " " "Let's start! :) ")
 print(" ")
 
 
 questions = {
-"What is Pyhon?" : "A",
-"Who created Python?" : "C",
-"What year Python was created?" : "B",
-"What is Python used for?" : "D",
-"Why is paython one of the most popular programming languages?" : "B",
-"How do we call @ symbol in Python?" : "C",
-"True or False are what data type in Python? " : "A",
-"What is the correct way to create a function in Python?" : "D",
-"What is the best web framework for Python?" : "D",
-"What is the correct file extension for Python files?" : "A",
-"Which statement is used to stop a loop" : "B",
-"What was inspration for Python name?" : "C",
-"What signifies the end of a statement block or suite in Python?" : "D",
-"How many CPU cores will the Python threading library take advantage of simultaneously?" : "C",
-"When a value is truncated to 3 decimal places, which of the following is true?" : "A"
-
+    "What is Pyhon?": "A",
+    "Who created Python?": "C",
+    "What year Python was created?": "B",
+    "What is Python used for?": "D",
+    "Why is paython one of the most popular programming languages?": "B",
+    "How do we call @ symbol in Python?": "C",
+    "True or False are what data type in Python? ": "A",
+    "What is the correct way to create a function in Python?": "D",
+    "What is the best web framework for Python?": "D",
+    "What is the correct file extension for Python files?": "A",
+    "Which statement is used to stop a loop": "B",
+    "What was inspration for Python name?": "C",
+    "What signifies the end of a statement block or suite in Python?": "D",
+    "How many CPU cores will the Python threading library take advantage of simultaneously?": "C",
+    "When a value is truncated to 3 decimal places, which of the following is true?": "A",
 }
-
 
 
 options = [
     ["A. Programming language", "B. Music genre", "C. Social network", "D. Food"],
     ["A. Nikola Tesla", "B. Bill Gates", "C. Guido van Rossum", "D. Thomas Edison"],
     ["A. 1865", "B. 1991", "C. 2015", "D. 2022"],
-    ["A. Web Development", "B. AI and machine learning", "C. Data analytics", "D. All of the above"],
-    ["A. Unnecessarily complicated", "B. Powerful yet easy to learn", "C. One of the oldest programming languages", "D. it's not popular at all"],
+    [
+        "A. Web Development",
+        "B. AI and machine learning",
+        "C. Data analytics",
+        "D. All of the above",
+    ],
+    [
+        "A. Unnecessarily complicated",
+        "B. Powerful yet easy to learn",
+        "C. One of the oldest programming languages",
+        "D. it's not popular at all",
+    ],
     ["A. At", "B. Monkey", "C. Decorator ", "D. Assignment Operator"],
     ["A. Boolean", "B. Integers", "C. Array", "D. Floats"],
     ["A. var", "B. let", "C. int", "D. def"],
     ["A. Laravel", "B. Swift", "C. NET", "D. Django"],
     ["A. py", "B. php", "C. js", "D. json"],
     ["A. stop", "B. brake", "C. exit", "D. pass"],
-    ["A. Snakes on the plane", "B. Medusa", "C. British comedy troupe", "D. Snake Island"],
-    ["A. Comment", "B. End", "C. }", "D. A line that is indented less than the previous line"],
+    [
+        "A. Snakes on the plane",
+        "B. Medusa",
+        "C. British comedy troupe",
+        "D. Snake Island",
+    ],
+    [
+        "A. Comment",
+        "B. End",
+        "C. }",
+        "D. A line that is indented less than the previous line",
+    ],
     ["A. Two", "B. None", "C. One", "D. All of the available cores"],
-    ["A. Positive numbers are rounded down and negative numbers are rounded up" , "B. Positive numbers are rounded up and negative numbers are rounded down.", "C. Both positive and negative numbers are rounded down.", "D. Both positive and negative numbers are rounded up."]
+    [
+        "A. Positive numbers are rounded down and negative numbers are rounded up",
+        "B. Positive numbers are rounded up and negative numbers are rounded down.",
+        "C. Both positive and negative numbers are rounded down.",
+        "D. Both positive and negative numbers are rounded up.",
+    ],
 ]
+
 
 def start_game():
     """
@@ -70,7 +96,6 @@ def start_game():
     correct_guesses = 0
     question_num = 1
 
-
     """
     Listing questions and options
 
@@ -79,9 +104,9 @@ def start_game():
     for key in questions:
         print("")
         print(key)
-        for i in options[question_num-1]:
+        for i in options[question_num - 1]:
             print(i)
-        
+
         """
         Capturing user input and checking the answer
 
@@ -109,6 +134,7 @@ def start_game():
 
     display_score(correct_guesses, guesses)
 
+
 def check_answer(answer, guess):
     if answer == guess:
         print(" ")
@@ -118,17 +144,21 @@ def check_answer(answer, guess):
         print(" ")
         print("Unfortunately, that is not the correct answer.")
         print(" ")
-        print("Your answer is " + guess + " And the correct answer is " + answer )
+        print("Your answer is " + guess + " And the correct answer is " + answer)
         print(" ")
         print("Game Over")
         print(" ")
         play_again()
 
+
 def display_score(correct_guesses, guesses):
-    score = int((correct_guesses/len(questions)))
-    print("Those are all 15 questions i had for you, congratulations you won a million euros (not really) :D ")
+    score = int((correct_guesses / len(questions)))
+    print(
+        "Those are all 15 questions i had for you, congratulations you won a million euros (not really) :D "
+    )
     print(" ")
     play_again()
+
 
 def play_again():
     play = input("Would you like to play again? (Yes or No) ")
@@ -138,6 +168,7 @@ def play_again():
     else:
         print("Okay " + name + " Thank you for playing")
         quit()
+
 
 welcome_player()
 
